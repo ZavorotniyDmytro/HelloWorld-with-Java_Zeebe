d9237316b616b260a4211c3ff933b2c30f284d2d
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-07-26 02:31:09 +0300

    success test fs-connector (only upload)
    but File not deleted

--- src/main/java/zeebe/camunda/introduction/Main.java
+++ src/main/java/zeebe/camunda/introduction/Main.java
@@ -4,6 +4,8 @@ import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,9 +16,7 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableScheduling
-//@Deployment(resources = "classpath:message-bpmn-diagram.bpmn")
 public class Main {
-
     static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     static ConsoleHandler handler = new ConsoleHandler();
     static ZeebeClient zeebeClient = null;
@@ -25,43 +25,39 @@ public class Main {
         SpringApplication app = new SpringApplication(Main.class);
         app.run(args);
 
-        final String ADDRESS = "172.17.201.28:26500";
-
         logger.setLevel(Level.ALL);
         handler.setLevel(Level.ALL);
         logger.addHandler(handler);
-        logger.info("SERVER STARTED");
 
+        final String ADDRESS = "172.17.201.28:26500";
         final ZeebeClientBuilder clientBuilder;
         clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(ADDRESS).usePlaintext();
 
         try (final ZeebeClient client = clientBuilder.build()) {
-
             zeebeClient = client;
             final DeploymentEvent deploymentEvent = deployDiagram(client);
 
             System.out.println("Deployment created with key: " + deploymentEvent.getKey());
             System.out.println("Deployment created with Processes: " + deploymentEvent.getProcesses());
 
-//            ProcessInstanceEvent instance = createInstance(client);
-//
-//            System.out.println("Instance created with ProcessId: " + instance.getBpmnProcessId());
-//            System.out.println("Instance created with ProcessDefinitionKey: " + instance.getProcessDefinitionKey());
-//            System.out.println("Instance created with ProcessInstanceKey: " + instance.getProcessInstanceKey());
+            ProcessInstanceEvent instance = createInstance(client);
 
+            System.out.println("Instance created with ProcessId: " + instance.getBpmnProcessId());
+            System.out.println("Instance created with ProcessDefinitionKey: " + instance.getProcessDefinitionKey());
+            System.out.println("Instance created with ProcessInstanceKey: " + instance.getProcessInstanceKey());
         }
     }
 
     static DeploymentEvent deployDiagram(ZeebeClient client){
         return client.newDeployResourceCommand()
-                .addResourceFromClasspath("message-bpmn-diagram-v2.bpmn")
+                .addResourceFromClasspath("using-fs-connector.bpmn")
                 .send()
                 .join();
     }
 
     static ProcessInstanceEvent createInstance(ZeebeClient client){
         return client.newCreateInstanceCommand()
-                .bpmnProcessId("MessageHandler")
+                .bpmnProcessId("FS-connector")
                 .latestVersion()
                 .send()
                 .join();
