6159e8e2a871971173cda2efde86233b66b64814
ZavorotniyDmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-09-08 17:08:10 +0300

    ADD: throw/catch message logic

--- src/main/java/zeebe/camunda/introduction/Main.java
+++ src/main/java/zeebe/camunda/introduction/Main.java
@@ -2,10 +2,17 @@ package zeebe.camunda.introduction;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,38 +21,43 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 @SpringBootApplication
 @EnableScheduling
-public class Main {
+// @Deployment(resources = "classpath:message-start.bpmn")
+public class Main // implements CommandLineRunner
+{
     static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     static ConsoleHandler handler = new ConsoleHandler();
-    static ZeebeClient zeebeClient = null;
+
+    @Autowired
+    private ZeebeClient client;
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
         app.run(args);
 
-        logger.setLevel(Level.ALL);
-        handler.setLevel(Level.ALL);
-        logger.addHandler(handler);
+//        logger.setLevel(Level.ALL);
+//        handler.setLevel(Level.ALL);
+//        logger.addHandler(handler);
 
-        final String ADDRESS = "172.17.201.28:26500";
-        final ZeebeClientBuilder clientBuilder;
-        clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(ADDRESS).usePlaintext();
+//        final String ADDRESS = "172.17.201.28:26500";
+//        final ZeebeClientBuilder clientBuilder;
+//        clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(ADDRESS).usePlaintext();
 
-        try (final ZeebeClient client = clientBuilder.build()) {
-            zeebeClient = client;
-            final DeploymentEvent deploymentEvent = deployDiagram(client);
-
-            System.out.println("Deployment created with key: " + deploymentEvent.getKey());
-            System.out.println("Deployment created with Processes: " + deploymentEvent.getProcesses());
-
-            ProcessInstanceEvent instance = createInstance(client);
-
-            System.out.println("Instance created with ProcessId: " + instance.getBpmnProcessId());
-            System.out.println("Instance created with ProcessDefinitionKey: " + instance.getProcessDefinitionKey());
-            System.out.println("Instance created with ProcessInstanceKey: " + instance.getProcessInstanceKey());
-        }
+//        try (final ZeebeClient client = clientBuilder.build()) {
+//            zeebeClient = client;
+//            final DeploymentEvent deploymentEvent = deployDiagram(client);
+//
+//            System.out.println("Deployment created with key: " + deploymentEvent.getKey());
+//            System.out.println("Deployment created with Processes: " + deploymentEvent.getProcesses());
+//
+//            ProcessInstanceEvent instance = createInstance(client);
+//
+//            System.out.println("Instance created with ProcessId: " + instance.getBpmnProcessId());
+//            System.out.println("Instance created with ProcessDefinitionKey: " + instance.getProcessDefinitionKey());
+//            System.out.println("Instance created with ProcessInstanceKey: " + instance.getProcessInstanceKey());
+//        }
     }
 
     static DeploymentEvent deployDiagram(ZeebeClient client){
@@ -63,4 +75,32 @@ public class Main {
                 .join();
     }
 
+//    @JobWorker(type = "print-local-msg")
+//    public void logMessage(ActivatedJob job, JobClient jobClient){
+//        System.out.println(job.getVariables());
+//        jobClient.newCompleteCommand(job).send();
+//    }
+//
+//    @JobWorker(type = "throw-message")
+//    public void throwMessage(ActivatedJob job, JobClient jobClient, @Variable String messageName){
+//        System.out.println(job.getVariables());
+//
+//        client.newPublishMessageCommand()
+//                .messageName(messageName)
+//                .correlationKey("end-1")
+//                .send();
+//        jobClient.newCompleteCommand(job).send();
+//    }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        client.newPublishMessageCommand()
+//                .messageName("start")
+//                .correlationKey("start-1")
+//                .send()
+//                .exceptionally(throwable -> {
+//                    throw new RuntimeException("could not publish message", throwable);
+//                });
+//    }
 }
--- src/main/java/zeebe/camunda/introduction/MessageService.java
+++ src/main/java/zeebe/camunda/introduction/MessageService.java
@@ -1,18 +1,60 @@
 package zeebe.camunda.introduction;
 
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.Variable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import static java.lang.System.out;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class MessageService {
+    static Logger logger = Logger.getLogger(MessageService.class.getName());
 
-    @JobWorker(type = "handling_message")
-    public void logMessage(ActivatedJob job, @Variable String text_field){
-        out.println(text_field);
+    @Autowired
+    private ZeebeClient client;
+
+    @JobWorker(type = "throw-message", fetchAllVariables = true, autoComplete = false)
+    public void throwMessage(ActivatedJob job, JobClient jobClient,
+                             @Variable String messageName,
+                             @Variable String sessionId){
+        // System.out.println("throw-message " + job.getVariablesAsMap());
+
+        Map<String, Object> variables = job.getVariablesAsMap();
+        logger.log(Level.INFO,
+                "[throw-message:" + variables.toString() + ", processInstanceID=" +
+                        job.getProcessInstanceKey() + "]");
+
+        client.newPublishMessageCommand()
+                .messageName(messageName)
+                .correlationKey(
+                        sessionId
+                )
+                .variables(variables)
+                .send()
+                .join();
+        jobClient.newCompleteCommand(job.getKey()).variables(variables).send().join();
+    }
+
+    @JobWorker(type = "key-generate", fetchAllVariables = true, autoComplete = false)
+    public void keyGenerate(ActivatedJob job, JobClient jobClient){
+
+        Map<String, Object> variables = job.getVariablesAsMap();
+        variables.put("sessionId", UUID.randomUUID());
+
+       // System.out.println("key-generate " + variables);
+        logger.log(Level.INFO,
+                "[key-generate:" + variables.toString() + ", processInstanceID=" +
+                        job.getProcessInstanceKey() + "]");
+
+        jobClient.newCompleteCommand(job).variables(variables).send().join();
     }
 
 }
